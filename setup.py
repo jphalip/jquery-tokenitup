836da37f9c28e73ac36e805477fc4fdb71039bb7
Julien Phalip <jphalip@gmail.com>
2010-09-19 17:16:45 +1000

    Fixed import in setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,5 @@
+from setuptools import setup
+
+# This is not a Python package, but we put a setup.py file anyway so that it can be pip installable. Kinda dirty but it works.
 setup(name='jquery-tokenitup',
       version='0.1',)
